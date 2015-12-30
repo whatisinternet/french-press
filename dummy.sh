5f320f503f488b4bed585b34a513acc8066a1ea0
Whatisinternet <joshteeter@gmail.com>
2015-12-30 18:11:41 -0500

    Added dummy generator

---
+++ dummy.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+./fp g --app --appName AwesomeApp --author YourName --ghUser GitHubUserName --email some@one.com
+./fp g --view --test1 --route root
+./fp g --view --test2 --route abc
+./fp g --view --test3 --route def --componentFolder testingMore
+./fp g --view --test4 --slim
+./fp g --component --test5 --folder test4 --slim
+./fp g --component --test6 --folder test4
