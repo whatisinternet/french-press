a4158a2eb8d3f1074b50a4b153d04173df95a6d8
Whatisinternet <joshteeter@gmail.com>
2016-01-20 20:43:07 -0500

    Fixed up config files to function as expected.

--- templates/webpack-config/base.config.js
+++ templates/webpack-config/base.config.js
@@ -17,12 +17,17 @@ module.exports = {
       { test: /\.coffee$/, loader: "coffee-loader" },
       { test: /\.(png|jpg)$/, loader: "file-loader?name=images/[name].[ext]" },
       { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url-loader?limit=10000&mimetype=application/font-woff" },
-      { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader"},
+      { test: /\.(ttf|eot|svg)(\?v=[1-9]\.[0-9]\.[0-9])?$/, loader: "file-loader"},
+      { test: /\.woff(2)?\?\-hdf3wh?$/, loader: "url-loader?limit=10000&mimetype=application/font-woff" },
+      { test: /\.(ttf|eot|svg)\?\-hdf3wh$/, loader: "url-loader"},
+      { test: /\.(ttf|eot|svg)$/, loader: "url-loader"},
+      { test: /\.woff$/i, loader: "url-loader?limit=20000&mimetype=application/font-woff" },
+      { test: /\.css$/, loader: "style!css" },
       { test: /\.sass$/, loader: "style-loader!css-loader!sass?indentedSyntax" }
     ]
   },
   resolve: {
-    extensions: ["", ".js", ".coffee", ".sass", "eot", "svg", "ttf", "woff", "woff2"]
+    extensions: ["", ".js", ".coffee", ".sass", ".css",  "eot", "svg", "ttf", "woff", "woff2"]
   },
 
   plugins: [
