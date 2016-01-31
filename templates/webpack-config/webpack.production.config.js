e05404f9caf261e47c747219ef8ae9892de85611
Whatisinternet <joshteeter@gmail.com>
2016-01-31 14:25:27 -0500

    Patched up production webpack config

--- templates/webpack-config/webpack.production.config.js
+++ templates/webpack-config/webpack.production.config.js
@@ -1,2 +1,9 @@
 var base = require('./base.config.js');
+base.entry = {full: './app/index.coffee'}
+base.output = {path: './dist', publicPath: '/', filename: 'app.entry.js'}
+base.plugins = [new webpack.ProvidePlugin({
+  "React": "react",
+  "ReactDOM": "react-dom",
+  "_": "lodash",
+  "Bemmer": "bemmer-node/bemmer-class"})]
 module.exports = base
